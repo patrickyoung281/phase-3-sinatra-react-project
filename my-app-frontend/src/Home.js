2335c8f8c7009426bddf32af5d3c5e7de38eccac
Patrick Young <patrickyoung281@gmail.com>
2022-12-07 15:12:10 -0500

    Home Page Styled

--- my-app-frontend/src/Home.js
+++ my-app-frontend/src/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
+import bannerworldflags from "./images/bannerworldflags.jpg";
 
 function Home () {
 
     return (
         <div>
-            <h2 className="headerText">Herzlich Willkommen!</h2>
+            <h1 className="headerText">Herzlich Willkommen!</h1>
+            <h2>Welcome to Patrick's Language Database!</h2>
+            <h3>This database keeps tracks of languages, their dialects, and how many people speak a given language.</h3>
+            <h3>To view or add a language to the database, simply click the "Languages" button above!</h3>
+            <img id="homeImage" src={bannerworldflags} alt = "World Flags"/>
+            
         </div>
     )
 }
