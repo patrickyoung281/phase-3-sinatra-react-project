448518b1187dc6d97ed17ef421e9d9f28a148094
Patrick Young <patrickyoung281@gmail.com>
2022-12-06 20:22:19 -0500

    useHistory hook added to delete button

--- my-app-frontend/src/DeleteLanguage.js
+++ my-app-frontend/src/DeleteLanguage.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-function DeleteLanguage ( {displayLanguages, setDisplayLanguages, id} ) {
+function DeleteLanguage ( { displayLanguages, setDisplayLanguages, id} ) {
     
+  const history = useHistory();
+  console.log("history", history);
+  const redirect = () => {
+    history.push('/languages/')
+  }
+  console.log(redirect)
+
     function handleDeleteClick(id) {
         fetch(`http://localhost:9292/languages/${id}`, {
           method: "DELETE",
@@ -14,7 +22,9 @@ function DeleteLanguage ( {displayLanguages, setDisplayLanguages, id} ) {
         const newList = displayLanguages.filter(entry=>{
           return entry.id !== deletedLanguage.id
         })
+        redirect()
         setDisplayLanguages(newList);
+        
       }
 
     return (
